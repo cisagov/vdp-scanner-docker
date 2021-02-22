2aa49e44978d33c8e905adb0b147267ca7582f0e
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-02-21 22:47:39 -0500

    Pare testing down to version checks
    The nature of this Docker project does not lend itself well to runtime testing,
    so for the time being I have pared testing down to version checks only.

--- tests/container_test.py
+++ tests/container_test.py
@@ -3,17 +3,10 @@
 
 # Standard Python Libraries
 import os
-import time
 
 # Third-Party Libraries
 import pytest
 
-ENV_VAR = "ECHO_MESSAGE"
-ENV_VAR_VAL = "Hello World from docker-compose!"
-READY_MESSAGE = "This is a debug message"
-SECRET_QUOTE = (
-    "There are no secrets better kept than the secrets everybody guesses."  # nosec
-)
 RELEASE_TAG = os.getenv("RELEASE_TAG")
 VERSION_FILE = "src/version.txt"
 
@@ -24,35 +17,6 @@ def test_container_count(dockerc):
     assert (
         len(dockerc.containers(stopped=True)) == 2
     ), "Wrong number of containers were started."
-
-
-def test_wait_for_ready(main_container):
-    """Wait for container to be ready."""
-    TIMEOUT = 10
-    for i in range(TIMEOUT):
-        if READY_MESSAGE in main_container.logs().decode("utf-8"):
-            break
-        time.sleep(1)
-    else:
-        raise Exception(
-            f"Container does not seem ready.  "
-            f'Expected "{READY_MESSAGE}" in the log within {TIMEOUT} seconds.'
-        )
-
-
-def test_wait_for_exits(main_container, version_container):
-    """Wait for containers to exit."""
-    assert main_container.wait() == 0, "Container service (main) did not exit cleanly"
-    assert (
-        version_container.wait() == 0
-    ), "Container service (version) did not exit cleanly"
-
-
-def test_output(main_container):
-    """Verify the container had the correct output."""
-    main_container.wait()  # make sure container exited if running test isolated
-    log_output = main_container.logs().decode("utf-8")
-    assert SECRET_QUOTE in log_output, "Secret not found in log output."
 
 
 @pytest.mark.skipif(
